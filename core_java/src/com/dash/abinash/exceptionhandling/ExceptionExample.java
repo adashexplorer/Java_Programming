6aae0eb26f207b9bc7018fda323062890021691e
Abinash Dash <abinashdashvssut@gmail.com>
2025-01-12 13:40:38 +0530

    Added output to the ExceptionExample.java program

--- core_java/src/com/dash/abinash/exceptionhandling/ExceptionExample.java
+++ core_java/src/com/dash/abinash/exceptionhandling/ExceptionExample.java
@@ -9,3 +9,11 @@ public class ExceptionExample {
         System.out.println(a/0);
     }
 }
+
+/**
+ * /Library/Java/JavaVirtualMachines/jdk-23.jdk/Contents/Home/bin/java -javaagent:/Applications/IntelliJ IDEA CE.app/Contents/lib/idea_rt.jar=51788:/Applications/IntelliJ IDEA CE.app/Contents/bin -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath /Users/adash/Hustle/Java_Programming/out/production/core_java com.dash.abinash.exceptionhandling.ExceptionExample
+ * Exception in thread "main" java.lang.ArithmeticException: / by zero
+ * 	at com.dash.abinash.exceptionhandling.ExceptionExample.main(ExceptionExample.java:9)
+ *
+ * Process finished with exit code 1
+ */
